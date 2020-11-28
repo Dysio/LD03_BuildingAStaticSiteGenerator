4b8eed6923e58150e20c9a2dbbc186f3ac7fd117
Dysio <td.dyszewski@gmail.com>
2020-11-28 20:29:35 +0100

    Generating Static Content: Compressing Our Static Files

--- prototypes.py
+++ prototypes.py
@@ -16,16 +16,21 @@ settings.configure(
     INSTALLED_APPS=(
         'django.contrib.staticfiles',
         'sitebuilder',
+        'compressor',
     ),
     STATIC_URL='/static/',
     SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR,'pages'),
     SITE_OUTPUT_DIRECTORY=os.path.join(BASE_DIR, '_build'),
     STATIC_ROOT=os.path.join(BASE_DIR, '_build', 'static'),
-    STATICFILES_STORAGE='django.contrib.staticfiles.storage.CachedStaticFilesStorage',
+    STATICFILES_FINDERS=(
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'compressor.finders.CompressorFinder',
+    ),
     TEMPLATES=[
         {
             'BACKEND':'django.template.backends.django.DjangoTemplates',
-            'DIRS':['templates'],
+            'DIRS':[],
             'APP_DIRS':True,
         }
     ],
